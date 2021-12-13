2df29a023d57fe14590bfed10cc2774dca28954a
Rootdiv <rootdiv@mail.ru>
2021-12-13 23:22:44 +0300

    fix: sidebar, widget and search page

--- home.php
+++ home.php
@@ -93,6 +93,7 @@
           <div class="col-lg-4">
             <div class="row">
               <div class="col-lg-12">
+                <?=get_search_form();?>
                 <?php if (!dynamic_sidebar('sidebar-blog')) : dynamic_sidebar('sidebar-blog'); endif; ?>
               </div>
               <div class="col-lg-12">
