c54a77bfb5790c60f4e025cacacc1d87970b0e9f
Rootdiv <rootdiv@mail.ru>
2021-12-20 17:37:25 +0300

    feat: comments submission form

--- comments.php
+++ comments.php
@@ -42,64 +42,55 @@
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	$defaults = [
+    'fields'               => [
+      'author' => '<div class="comment-form-author form-group mb-3 col-lg-6">
+        <input id="author" name="author" type="text" class="form-control" placeholder="Имя"
+          value="' . esc_attr($commenter['comment_author']) . '" size="30" />
+      </div>',
+      'email'  => '<div class="form-group mb-4 col-lg-6">
+        <input id="email" name="email" type="email" class="form-control" placeholder="Email"
+          value="' . esc_attr( $commenter['comment_author_email']) . '" size="30" aria-describedby="email-notes" />
+      </div>',
+      'cookies' => '<div class="comment-form-cookies-consent d-flex col-lg-12">' .
+        sprintf( '<input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" class="mr-3" type="checkbox" value="yes"%s />', @$consent ) . '
+        <label for="wp-comment-cookies-consent">'. __( 'Save my name, email, and website in this browser for the next time I comment.' ) . '</label>
+      </div>',
+    ],
+    'comment_field'        => '<div class="form-group mb-3 col-lg-12">
+      <textarea id="comment" name="comment" cols="30" rows="6" class="form-control" placeholder="Комментарий"
+        aria-required="true" required="required"></textarea>
+    </div>',
+    'must_log_in'          => '<div class="must-log-in mb-3 col-lg-12">' .
+      sprintf(__('Вам нужно <a href="%s">войти</a> чтобы оставить комментарий.'), wp_login_url(apply_filters('the_permalink', get_permalink($post->ID)))) . '
+    </div>',
+    'logged_in_as'         => '<div class="logged-in-as mb-3 col-lg-12">' .
+      sprintf(__('<a href="%1$s" aria-label="Вы вошли как %2$s.">Вы вошли как %2$s</a>. <a href="%3$s">Выйти?</a>'),
+        get_edit_user_link(), $user_identity, wp_logout_url(apply_filters('the_permalink', get_permalink($post->ID)))) . '
+    </div>',
+    'comment_notes_before' => '<div class="comment-notes mb-4 col-lg-12">
+      <span id="email-notes">' . __('Ваш E-mail защищен от спама') . '</span>
+    </div>',
+    'comment_notes_after'  => '',
+    'id_form'              => 'commentform',
+    'id_submit'            => 'submit',
+    'class_form'           => 'comment-form row',
+    'class_submit'         => 'submit btn btn-hero btn-circled',
+    'name_submit'          => 'submit',
+    'title_reply'          => __('Оставьте комментарий'),
+    'title_reply_to'       => __('Ответить %s'),
+    'title_reply_before'   => '<h3 id="reply-title" class="comment-reply-title">',
+    'title_reply_after'    => '</h3>',
+    'cancel_reply_before'  => ' <small>',
+    'cancel_reply_after'   => '</small>',
+    'cancel_reply_link'    => __('Отменить отправку'),
+    'label_submit'         => __('Отправить комментарий'),
+    'submit_button'        => '<button name="%1$s" type="submit" id="%2$s" class="%3$s">%4$s</button>',
+    'submit_field'         => '<div class="form-submit col-lg-12">%1$s %2$s</div>',
+    'format'               => 'html5',
+  ];
+
+  comment_form($defaults);
 	?>
 
 </div><!-- #comments -->
-<div class="comments my-4">
-  <h3 class="mb-5">Комментарии:</h3>
-  <div class="media mb-4">
-    <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-    <div class="media-body">
-      <h5>Антон Колесников</h5>
-      <span class="text-muted">20 января 2020</span>
-      <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores
-        quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
-      <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-      <div class="media mt-5">
-        <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-        <div class="media-body">
-          <h5>Егор Савицкий</h5>
-          <span class="text-muted">20 января 2020</span>
-          <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores
-            quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
-          <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="media mb-4">
-    <img src="images/blog/2.jpg" alt="" class="img-fluid d-flex mr-4 rounded">
-    <div class="media-body">
-      <h5>Валентин Крашков</h5>
-      <span class="text-muted">14 февраля 2020</span>
-      <p class="mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi laborum dolores
-        quidem ea optio fuga nesciunt tempora, in tenetur iusto!</p>
-      <a href="#" class="reply">Ответить <i class="fa fa-reply"></i></a>
-    </div>
-  </div>
-</div>
-<div class="mt-5 mb-3">
-  <h3 class="mt-5 mb-2">Оставьте комментарий</h3>
-  <p class="mb-4">Ваш E-mail защищен от спама</p>
-  <form action="#" class="row">
-    <div class="col-lg-12">
-      <div class="form-group mb-3">
-        <textarea cols="30" rows="6" class="form-control" placeholder="Комментарий"></textarea>
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <div class="form-group mb-3">
-        <input type="text" class="form-control" placeholder="Имя">
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <div class="form-group mb-4">
-        <input type="email" class="form-control" placeholder="Email">
-      </div>
-    </div>
-    <div class="col-lg-12">
-      <a href="#" class="btn btn-hero btn-circled">Оставить комментарий</a>
-    </div>
-  </form>
-</div>
