247275acdeb3715f2b71908dc32c334c56793269
Rootdiv <rootdiv@mail.ru>
2022-08-07 19:16:55 +0300

    fix: the translate in comments.php

--- comments.php
+++ comments.php
@@ -36,7 +36,7 @@
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if (!comments_open()) :
 			?>
-			<p class="no-comments"><?php esc_html_e('Comments are closed.', 'band-digital'); ?></p>
+			<p class="no-comments"><?php esc_html_e('Comments are closed.', 'band_digital'); ?></p>
 			<?php
 		endif;
 
